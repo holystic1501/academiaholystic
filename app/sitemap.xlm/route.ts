6ec7deaac4e7fd97ba8fe47c6886c00b81b339a9
holystic1501 <holysticacademy@gmail.com>
2025-11-03 23:27:47 -0500

    no funka el sitemap

---
+++ app/sitemap.xlm/route.ts
@@ -0,0 +1,30 @@
+export async function GET() {
+  const urls = [
+    "https://academiaholystic.vercel.app/",
+    "https://academiaholystic.vercel.app/nosotros",
+    "https://academiaholystic.vercel.app/contacto",
+  ];
+
+  const lastMod = new Date().toISOString().split(".")[0] + "Z";
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `
+  <url>
+    <loc>${url}</loc>
+    <lastmod>${lastMod}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
